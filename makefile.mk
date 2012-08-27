cab4820eaa89eb2a44f88da147975ec42de7bdb5
Golevka <xu_michelle@live.com>
2012-08-27 19:10:26 +0800

    make it compile on OS X, thank jaeschliman for reporting this issue

--- makefile.mk
+++ makefile.mk
@@ -31,14 +31,12 @@ CFLAGS += $(addprefix  -I, $(INCLUDE_PATH))
 # Build external library cmdline parameter, those -Xlinker directives instructs
 # the linker find the correct linking sequence regardless the order of items
 # specified in EXTERNAL_LIBS.
-LOADLIBES += \
-	-Xlinker --start-group \
+LOADLIBS += \
 		$(addprefix  -Xlinker , $(EXTERNAL_LIBS)) \
-	-Xlinker --end-group
 
 # PROGRAM_NAME is provided in custom makefile
 $(PROGRAM_NAME): $(object-list)
-	$(LINK.c) $^ $(LOADLIBES) $(LDLIBS) -o $@
+	$(LINK.c) $^ $(LOADLIBS) $(LDLIBS) -o $@
 
 
 $(OBJECT_PATH)/%.o: %.c
